60602a28ed307462483fe733db98ade8596a975d
devmatias <devmatias@gmail.com>
2022-06-18 11:56:07 -0300

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Matias Correa Franco de Faria on 18/06/22.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cards
+//
+//  Created by Matias Correa Franco de Faria on 18/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
